991c96b4e5866cdc57ac365ce546d9ab89b4abcb
VIper17052001 <79989185+VIper17052001@users.noreply.github.com>
2024-01-26 22:00:33 +0200

    feature/section-hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "./components/Header/Header.tsx";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer.tsx";
+import { Landing } from "./components/Landing/Landing.tsx";
 
 export const App = () => {
   return (
