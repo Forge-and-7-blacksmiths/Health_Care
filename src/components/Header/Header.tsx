b75086ec5e62106f57d1ad364f9e807e8109dd27
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2024-01-28 01:30:40 +0200

    loginpage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,10 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <div className="testLink">
-      <NavLink to="signup" className="navbarItem">
+      <NavLink to="signin" className="navbarItem">
         Sign in
       </NavLink>
+      
     </div>
   );
 };
