4b5cecf567f23ad2acbf22d42689228c7b645bd7
ZakZanko <zakzanko2112@gmail.com>
2024-01-28 21:00:51 +0200

    fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,68 @@
 import React from "react";
-import './Header.scss';
-import { NavLink } from "react-router-dom";
+
+import "./Header.scss";
+import logoIcon from "../../images/logoIcon.svg";
+import logoText from "../../images/logoText.png";
 
 export const Header = () => {
   return (
-    <div className="testLink">
-      <NavLink to="signin" className="navbarItem">
-        Sign in
-      </NavLink>
-      
-    </div>
+    <header className="header">
+      <div className="header__container">
+        <div className="header__first-block">
+          <a href="/" className="logo__link">
+            <img className={"logoIcon"} src={logoIcon} alt="logoIcon" />
+          </a>
+          <a href="/" className="logo__text--link">
+            <img
+              className={"logoText logoText__left"}
+              src={logoText}
+              alt="logoText"
+            />
+          </a>
+        </div>
+        <nav className="menu__nav">
+          <ul className="menu__nav--list list ul">
+            <li className="menu__nav--item list">
+              <a href="/" className="menu__nav--link link">
+                Home
+              </a>
+            </li>
+            <li className="menu__nav--item list">
+              <a href="/" className="menu__nav--link link">
+                About Us
+              </a>
+            </li>
+            <li className="menu__nav--item list">
+              <a href="" className="menu__nav--link link">
+                How to use
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="log__in">
+          <ul className="log__in--list ul">
+            <li className="log__in--item list">
+              <a href="/" className="log__nav--link link">
+                English
+              </a>
+            </li>
+            <li className="log__in--item list">
+              <a href="/" className="log__nav--link link">
+                Sign in
+              </a>
+            </li>
+            <li className="log__in--item  list">
+              <a
+                href="/"
+                className="log__nav--link log__in--item-register  link"
+              >
+                Register
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
